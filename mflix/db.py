2db38ce53b4ea5f81c9b0424852cdaf9e3aa412e
skolesnikov <chimbosan@yandex.ru>
2021-05-28 17:46:53 +0300

    Chapter 2: User-Facing Backend, Ticket: Delete Comments

--- mflix/db.py
+++ mflix/db.py
@@ -321,21 +321,7 @@ def update_comment(comment_id, user_email, text, date):
 
 
 def delete_comment(comment_id, user_email):
-    """
-    Given a user's email and a comment ID, deletes a comment from the comments
-    collection
-    """
-
-    """
-    Ticket: Delete Comments
-
-    Match the comment_id and user_email with the correct fields, to make sure
-    this user has permission to delete this comment, and then delete it.
-    """
-
-    # TODO: Delete Comments
-    # Use the user_email and comment_id to delete the proper comment.
-    response = db.comments.delete_one( { "_id": ObjectId(comment_id) } )
+    response = db.comments.delete_one({"_id": ObjectId(comment_id), "email": user_email})
     return response
 
 
